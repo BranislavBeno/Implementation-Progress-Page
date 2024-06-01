092baef6ab7c89efd8ce16ea96402713d66a54f7
Branislav Beno <brano.beno@gmail.com>
2024-06-01 17:27:42 +0200

    refactor: Use OpenRewrite to migrate to Spring Bot 3.3

--- src/main/java/com/progress/application/project/ProjectApplication.java
+++ src/main/java/com/progress/application/project/ProjectApplication.java
@@ -3,7 +3,6 @@ package com.progress.application.project;
 import com.progress.application.project.service.IssueFetchingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -16,7 +15,7 @@ public class ProjectApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ProjectApplication.class);
 
-    ProjectApplication(@Autowired WebPageProvider pageProvider) {
+    ProjectApplication(WebPageProvider pageProvider) {
         try {
             Path destination = Paths.get("public/index.html");
             pageProvider.provideWebPage(destination);
